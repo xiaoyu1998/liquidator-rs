c9b1fe1adb2f91eaea04051228c66000aaba7038
xiaoyu1998 <1634054493@qq.com>
2024-07-13 18:47:43 +0800

    update

--- crates/liquidator-contract/contracts/Liquidator.sol
+++ crates/liquidator-contract/contracts/Liquidator.sol
@@ -25,26 +25,26 @@ contract Liquidator is Ownable, IUniswapV3SwapCallback {
     address internal immutable dataStore;
     address internal immutable liquidationHandler;
 
-    function toString(bytes memory data) internal pure returns(string memory) {
-        bytes memory alphabet = "0123456789abcdef";
+    // function toString(bytes memory data) internal pure returns(string memory) {
+    //     bytes memory alphabet = "0123456789abcdef";
 
-        bytes memory str = new bytes(2 + data.length * 2);
-        str[0] = "0";
-        str[1] = "x";
-        for (uint i = 0; i < data.length; i++) {
-            str[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
-            str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
-        }
-        return string(str);
-    }
+    //     bytes memory str = new bytes(2 + data.length * 2);
+    //     str[0] = "0";
+    //     str[1] = "x";
+    //     for (uint i = 0; i < data.length; i++) {
+    //         str[2+i*2] = alphabet[uint(uint8(data[i] >> 4))];
+    //         str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+    //     }
+    //     return string(str);
+    // }
 
-    function toString(address account) internal pure returns(string memory) {
-        return toString(abi.encodePacked(account));
-    }
+    // function toString(address account) internal pure returns(string memory) {
+    //     return toString(abi.encodePacked(account));
+    // }
 
-    function toString(uint256 value) public pure returns(string memory) {
-        return toString(abi.encodePacked(value));
-    }
+    // function toString(uint256 value) public pure returns(string memory) {
+    //     return toString(abi.encodePacked(value));
+    // }
 
     constructor(
         address _factory, 
